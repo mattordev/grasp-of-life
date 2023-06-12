4f34afd3d47eb8e93f079ec4d15d747dbb7733e4
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-06-13 00:33:44 +0100

    feat: added temporary enemy manager script

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace GraspofLife
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        private int _healthpoints;
+
+        private void Awake()
+        {
+            _healthpoints = 30;
+        }
+
+        public bool TakeHit()
+        {
+            _healthpoints -= 10;
+            Debug.Log("Took some damage!");
+            bool isDead = _healthpoints <= 0;
+            if (isDead) _Die();
+            return isDead;
+        }
+
+        private void _Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
